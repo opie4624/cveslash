99f42de9406a9c37cdbb8c79724ddfffa277aed0
Andrew Kraut <akraut@opie.at>
2015-11-07 18:46:28 -0800

    fix typo in httpc.request return structure

--- lib/cveupdater.ex
+++ lib/cveupdater.ex
@@ -3,7 +3,7 @@ defmodule Cveupdater do
   def fetch_body(url) do
     :ssl.start
     :inets.start
-    {:ok, {_, 200, _}, _, body} = :httpc.request(url)
-    body
+    {:ok, {{_, 200, _}, _, body}} = :httpc.request(url)
+		body
   end
 end
