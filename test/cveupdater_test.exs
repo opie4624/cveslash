654cf9391a6fdb7867661d3a68a8ff8196cfb81a
Andrew Kraut <akraut@opie.at>
2015-11-07 18:47:20 -0800

    add tests for fetch_body

--- test/cveupdater_test.exs
+++ test/cveupdater_test.exs
@@ -5,4 +5,12 @@ defmodule CveupdaterTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+	test "retrieves a file from http" do
+		assert Regex.match?(~r/examples/i, to_string(Cveupdater.fetch_body('http://www.example.com/')))
+	end
+
+	test "retrieves a file from https" do
+		assert Regex.match?(~r/examples/i, to_string(Cveupdater.fetch_body('https://www.example.com/')))
+	end
 end
