279e9c526f7a4c6487cf7a11f3c35f626434b0c3
Andrew Kraut <akraut@gmail.com>
2015-11-14 20:29:52 -0800

    Switch to SweetXml xpath

--- lib/cveupdater.ex
+++ lib/cveupdater.ex
@@ -1,7 +1,6 @@
-defmodule Cveupdater do
-	require Record
 
-	for {record_name, record_definition} <- Record.extract_all(from_lib: "xmerl/include/xmerl.hrl"), do: Record.defrecord(record_name, record_definition)
+defmodule Cveupdater do
+	import SweetXml
 
 	@doc ~S"""
 	Returns a list of CVE Entries and their attributes given the URL or path to a GZ XML file.
@@ -15,13 +14,31 @@ defmodule Cveupdater do
 		:ssl.start
 		:inets.start
 		{:ok, {{_, 200, _}, _, body}} = :httpc.request(url)
-		for entry <- body
+		body
 		|> :zlib.gunzip
-		|> to_char_list
-		|> Exmerl.parse
-		|> Tuple.to_list
-		|> List.first
-		|> Exmerl.XPath.find("/nvd/entry"), do: xmlElement entry
-	end
-
+		|> xpath(
+			~x"//entry"l,
+			_id: ~x"./vuln:cve-id/text()",
+			summary: ~x"./vuln:summary/text()",
+			references: [
+				~x"./vuln:references"lo,
+				type: ~x"./@reference_type",
+				source: ~x"./vuln:source/text()",
+				reference: ~x"./vuln:reference/text()",
+				url: ~x"./vuln:reference/@href"
+			],
+			cvss: [
+				~x"./vuln:cvss/cvss:base_metrics"o,
+				score: ~x"./cvss:score/text()",
+				access_vector: ~x"./cvss:access-vector/text()",
+				access_complexity: ~x"./cvss:access-complexity/text()",
+				authentication: ~x"./cvss:authentication/text()",
+				confidentiality_impact: ~x"./cvss:confidentiality-impact/text()",
+				integrity_impact: ~x"./cvss:integrity-impact/text()",
+				availability_impact: ~x"./cvss:availability-impact/text()"
+			],
+			published: ~x"./vuln:published-datetime/text()",
+			modified: ~x"./vuln:last-modified-datetime/text()"
+		)
+	end	
 end
